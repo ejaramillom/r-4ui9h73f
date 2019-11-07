67e356129dbe0c16954673d565a86e12f9890a21
ejaramillom <ejaramilo@hotmail.com>
2019-11-06 22:43:07 -0500

    first commit

--- src/App.js
+++ src/App.js
@@ -2,14 +2,26 @@ import React, { Component } from 'react';
 import './App.css';
 
 class App extends Component {
+  constructor (props){
+    super(props);
+    this.handleChange = this.handleChange.bind(this);
+    this.state = {
+      value:  ''
+    }
+  }
   render() {
     return (
       <div>
         <h1>Un repetidor</h1>
-        <input type="text" placeholder="Empieza a escribir algo" />
-        <p className="repeater"></p>
+        <input type="text" placeholder="Empieza a escribir algo" onChange={this.handleChange}/>
+        <p className="repeater">{this.state.value}</p>
       </div>
     );
+  }
+  handleChange(event){
+    this.setState({
+      value: event.target.value,
+    });
   }
 }
 
